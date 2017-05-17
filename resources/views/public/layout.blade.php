8322500c62ead448fc0dbfa519c911831606cb7c
jellyfish <thomas.lawanson@gmail.com>
2017-05-17 11:43:09 +0100

    mobile friendly

--- resources/views/public/layout.blade.php
+++ resources/views/public/layout.blade.php
@@ -1,9 +1,25 @@
 <!DOCTYPE html>
-<html>
+<html lang="{{ config('app.locale') }}">
 <head>
-  <title>Thomas' Portfolio</title>
-  @include('public.partials.styles')
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Thomas' Portfolio</title>
+
+    <!-- Styles -->
+    @include('public.partials.styles')
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
 </head>
+
 
 <body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
     @include('public.partials.nav.site')
